25dc7c22d8a0e998b7589ccd13c4f296ddd19706
Hajimara <show3rg@gmail.com>
2019-10-31 23:03:52 +0900

    News-Viewer

---
+++ news-viewer/src/pages/NewsPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Categories from '../components/Categories';
+import NewsList from '../components/NewsList';
+
+const NewsPage = ({ match }) => {
+
+    const category = match.params.category || 'all';
+
+    return (
+        <>
+            <Categories></Categories>
+            <NewsList category={category}></NewsList>
+        </>
+    )
+}
+
+export default NewsPage;
