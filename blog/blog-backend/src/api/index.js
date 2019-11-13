ac568518aeded5364ed18ded1462bf1967e8c8f4
park sujin <show3rg@gmail.com>
2019-11-14 01:01:14 +0900

    JWT-tutorial base

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,9 +1,10 @@
 import Router from "koa-router";
 import posts  from './posts';
+import auth from "./auth/index";
 
 const api = new Router();
 
 api.use("/posts", posts.routes());
-
+api.use("/auth", auth.routes());
 //라우트 내보내기
 export default api;
